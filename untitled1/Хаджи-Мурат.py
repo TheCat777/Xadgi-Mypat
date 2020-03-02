9270f6f33cba13aadaa7c04a3456fe0d7956dd47
Святослав <57014150+TheCat777@users.noreply.github.com>
2020-03-02 14:16:57 +0300

    Update Хаджи-Мурат.py
    Приветствие

--- untitled1/Хаджи-Мурат.py
+++ untitled1/Хаджи-Мурат.py
@@ -1,2 +1,13 @@
 import time
-print()
+
+
+def stop():
+  print("Для продолжения введите любой текст")
+  a = input()
+
+  
+print('Добро пожаловать в квестовый квест:')
+time.sleep(1)
+print('Хаджи-Мурат Альтернативные истории')
+time.sleep(1)
+stop()
